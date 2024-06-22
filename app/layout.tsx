7c9230b2a41ffd8e59470cb7cb7de9ed1b6eb933
Abdallah <abdallaahnoor@gmail.com>
2024-06-22 19:43:53 +0300

    create meeting

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import NextTopLoader from "nextjs-toploader";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -41,6 +42,7 @@ export default function RootLayout({
             speed={200}
             shadow="none"
           />
+          <Toaster />
         </body>
       </ClerkProvider>
     </html>
